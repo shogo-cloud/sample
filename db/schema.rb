5b91d6adf148576c130d659484b81bde9f726386
小野祥吾 <ONO@onosachiware-no-MacBook-Pro.local>
2015-03-12 19:14:27 +0900

    レスポンシブ実験

--- db/schema.rb
+++ db/schema.rb
@@ -11,20 +11,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150311170249) do
+ActiveRecord::Schema.define(version: 20150311165618) do
 
   create_table "entries", force: :cascade do |t|
-    t.text     "image_url",  limit: 65535
-    t.string   "name",       limit: 255
-    t.integer  "age",        limit: 4
-    t.integer  "height",     limit: 4
-    t.string   "hometown",   limit: 255
-    t.text     "url",        limit: 65535
-    t.datetime "created_at",               null: false
-    t.datetime "updated_at",               null: false
-    t.integer  "rate",       limit: 4
-    t.string   "phone",      limit: 255
-    t.string   "mail",       limit: 255
+    t.text     "image_url",           limit: 65535
+    t.string   "name",                limit: 255
+    t.integer  "age",                 limit: 4
+    t.integer  "height",              limit: 4
+    t.string   "hometown",            limit: 255
+    t.text     "url",                 limit: 65535
+    t.datetime "created_at"
+    t.datetime "updated_at"
+    t.integer  "rate",                limit: 4
+    t.text     "appeal",              limit: 65535
+    t.string   "avatar_file_name",    limit: 255
+    t.string   "avatar_content_type", limit: 255
+    t.integer  "avatar_file_size",    limit: 4
+    t.datetime "avatar_updated_at"
+    t.string   "phone",               limit: 255
+    t.string   "mail",                limit: 255
   end
 
 end
